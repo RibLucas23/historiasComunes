67153278ab837858a251c980871cb58ca2fefc23
lucas ribeiro <rds.lucas23@gmail.com>
2024-07-04 15:16:44 -0300

    fixed/favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import NavBar from './components/layout/NavBar';
 import Footer from './components/layout/Footer';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
+			<Head>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 			<body className={inter.className}>
 				<NavBar />
 				{children}
--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,23 @@ import About from './components/About';
 import Suscribite from './components/Suscribite';
 import CardsContainer from './components/CardsContainer';
 import BannerAlforja from './components/layout/BannerAlforja';
+import Head from 'next/head';
 
 export default function Home() {
 	return (
-		<main
-			className='flex flex-col  items-center px-4 md:px-16 lg:px-32  '
-			style={{ backgroundImage: 'url(/bg.png)', backgroundRepeat: 'repeat' }}
-		>
-			<BannerAlforja />
-			<CardsContainer />
-			<About />
-			<Suscribite />
-		</main>
+		<>
+			<main
+				className='flex flex-col  items-center px-4 md:px-16 lg:px-32  '
+				style={{
+					backgroundImage: 'url(/bg.png)',
+					backgroundRepeat: 'repeat',
+				}}
+			>
+				<BannerAlforja />
+				<CardsContainer />
+				<About />
+				<Suscribite />
+			</main>
+		</>
 	);
 }
